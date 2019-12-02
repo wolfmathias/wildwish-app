d96866f0afbb2e876e04bfa21daab74d08355b43
mplichta <bigcatplichta@gmail.com>
2019-12-02 14:43:42 -0700

    generated user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  # method copy/pasted, check that it works and tweak
+  def self.from_omniauth(auth)
+    # Either create a User record or update it based on the provider (Google) and the UID   
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.token = auth.credentials.token
+      user.expires = auth.credentials.expires
+      user.expires_at = auth.credentials.expires_at
+      user.refresh_token = auth.credentials.refresh_token
+    end
+  end
+
+end
