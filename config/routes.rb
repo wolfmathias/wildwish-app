251a34421bd2ba65b08121504cd53cfd32ba809e
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:32:48 -0700

    generated Toy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :toys
   resources :keepers
   resources :donors
   resources :animals
---
+++ spec/factories/toys.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :toy do
+    
+  end
+end
