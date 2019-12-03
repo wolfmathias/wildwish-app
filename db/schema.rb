293766ac1c47b7b4206798ae17462cd8c5fd6b1e
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:57:48 -0700

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_204803) do
+ActiveRecord::Schema.define(version: 2019_12_03_024822) do
+
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.string "bio"
+    t.integer "keeper_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["keeper_id"], name: "index_animals_on_keeper_id"
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.integer "donor_id", null: false
+    t.integer "wish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["donor_id"], name: "index_donations_on_donor_id"
+    t.index ["wish_id"], name: "index_donations_on_wish_id"
+  end
+
+  create_table "donors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_donors_on_user_id"
+  end
+
+  create_table "keepers", force: :cascade do |t|
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_keepers_on_user_id"
+  end
+
+  create_table "toys", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -33,6 +74,15 @@ ActiveRecord::Schema.define(version: 2019_12_02_204803) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wishes", force: :cascade do |t|
+    t.integer "animal_id"
+    t.integer "toy_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["animal_id"], name: "index_wishes_on_animal_id"
+    t.index ["toy_id"], name: "index_wishes_on_toy_id"
   end
 
 end
