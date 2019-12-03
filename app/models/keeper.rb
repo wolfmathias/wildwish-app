0e686874c8da70221244fd5f79a8c279ede88709
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:59:18 -0700

    added associations

--- app/models/keeper.rb
+++ app/models/keeper.rb
@@ -1,2 +1,9 @@
 class Keeper < ApplicationRecord
+    # associations
+    belongs_to :user
+    has_many :animals
+    has_many :wishes, through: :animals
+    has_many :toys, through: :wishes
+    accepts_nested_attributes_for :animals
+
 end
