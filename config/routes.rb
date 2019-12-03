c8b1b6ecf914b5e2cf97ab3e4b27a6e8263bcbdd
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:29:56 -0700

    generated Donor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :donors
   resources :animals
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
---
+++ spec/factories/donors.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :donor do
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+  end
+end
