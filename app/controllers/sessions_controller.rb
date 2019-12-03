ee6ce34a299a7996935dfb229a3272cf3afb8899
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:25:04 -0700

    moved home action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 class SessonsController < Devise::SessonsController
 
-    # def new
-    #     super
-    #     redirect_to user_path(@user)
-    # end
+    
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
-
+    before_action :authenticate_user!
     
+    def show
+
+    end
+
+    def home
+        if user_signed_in?
+            redirect_to user_path(current_user)
+        else
+            render '/landing'
+        end
+    end
+
 end
