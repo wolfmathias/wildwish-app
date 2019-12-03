f27384a09ba3d01e70afd38bf9db7b8b8584b04b
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:59:09 -0700

    added associations

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,2 +1,9 @@
 class Donor < ApplicationRecord
+    # associations
+    belongs_to :user
+    has_many :donations
+    has_many :wishes, through: :donations
+    has_many :toys, through: :wishes
+    has_many :animals, through: :wishes
+    
 end
