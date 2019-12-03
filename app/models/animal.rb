8eda400c8bd8ee217c2c882d11b9e7fb1ff9c428
mplichta <bigcatplichta@gmail.com>
2019-12-02 19:58:59 -0700

    added associations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,9 @@
 class Animal < ApplicationRecord
+    # associations
+    belongs_to :keeper
+    has_many :wishes
+    has_many :toys, through: :wishes
+    has_many :donations, through: :wishes
+    has_many :donors, through: :donations
+    
 end
