d3a38f6c380d5c677ed72a8e839dfa849aca43d8
mplichta <bigcatplichta@gmail.com>
2019-12-04 13:14:12 -0700

    added collection helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
 module UsersHelper
+      # collection methods
+  # user can have toys, animals, and wishes through a keeper or a donor role
+  # keepers can also be donors, something like user.animals is ambiguous
+  # use keeper_animals and donor_animals (same as calling user.keeper.animals or user.donor.animals)
+  def donor_animals
+    if self.donor?
+      self.donor.animals
+    end
+  end
+
+  def keeper_animals
+    if self.keeper?
+      self.keeper.animals
+    end
+  end
+  
 end
