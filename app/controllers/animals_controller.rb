2da32e57b1d5635adc26ff936276446b8c478023
mplichta <bigcatplichta@gmail.com>
2019-12-04 10:51:03 -0700

    added update method

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,7 +6,6 @@ class AnimalsController < ApplicationController
     end
 
     def create
-        # raise params.inspect
         @animal = current_user.keeper.animals.build(animal_params)
         if @animal.save
             redirect_to animal_path(@animal)
@@ -19,10 +18,20 @@ class AnimalsController < ApplicationController
         @animal = Animal.find_by(id: params[:id])
     end
 
+    
+    def update 
+        @animal = Animal.find_by(id: params[:id]) 
+        if @animal.update(animal_params)
+            redirect_to animal_path(@animal)
+        else
+            render 'animals/edit'
+        end
+    end
+
     private
 
     def animal_params
-        params.require(:animal).permit(:name, :species, :bio, toy_ids: [])
+        params.require(:animal).permit(:id, :name, :species, :bio, toy_ids: [])
     end
 
 end
