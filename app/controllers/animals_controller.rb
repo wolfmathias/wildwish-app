aba137b45f447e078662a615cec35a5e859d7293
mplichta <bigcatplichta@gmail.com>
2019-12-03 22:08:19 -0700

    new, create, show methods

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,28 @@
 class AnimalsController < ApplicationController
+    # add authentication and authorization checks before appropriate methods
+
+    def new
+        @animal = current_user.keeper.animals.build
+    end
+
+    def create
+        # raise params.inspect
+        @animal = current_user.keeper.animals.build(animal_params)
+        if @animal.save
+            redirect_to animal_path(@animal)
+        else
+            render 'animals/new'
+        end
+    end
+
+    def show
+        @animal = Animal.find_by(id: params[:id])
+    end
+
+    private
+
+    def animal_params
+        params.require(:animal).permit(:name, :species, :bio, toy_ids: [])
+    end
+
 end
