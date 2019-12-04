660a7b3866fd43028e225bcf5c31329ea5151a6f
mplichta <bigcatplichta@gmail.com>
2019-12-03 22:08:47 -0700

    new and create methods

--- app/controllers/keepers_controller.rb
+++ app/controllers/keepers_controller.rb
@@ -1,2 +1,42 @@
 class KeepersController < ApplicationController
+
+    def new
+        # use @keeper for instance variable to avoid conflict/confusion since current_user returns @user
+        if current_user.keeper
+            redirect_to keeper_path(current_user.keeper)
+        else
+            @keeper = current_user.build_keeper
+            @keeper.animals.build
+        end
+        
+    end
+
+    def create
+        @keeper = current_user.build_keeper(keeper_params)
+        if @keeper.save
+            redirect_to keeper_path(@keeper)
+        else
+            render 'keepers/new'
+        end
+    end
+
+    def show
+        
+    end
+
+    # def index
+    # end
+
+    private
+    def keeper_params
+        params.require(:keeper).permit(
+            animals_attributes: [
+                :name, 
+                :species, 
+                :bio, 
+                toy_ids: []
+            ]
+        )
+    end
+    
 end
