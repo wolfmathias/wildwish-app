82609bafb3b39e15a15825497aaec317fa1943ee
mplichta <bigcatplichta@gmail.com>
2019-12-03 22:07:54 -0700

    new and create methods

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,2 +1,12 @@
 class WishesController < ApplicationController
+
+    # display form for creating new wish, set animal for collection
+    def new
+
+    end
+
+    # to create from user filled form
+    def create
+
+    end
 end
