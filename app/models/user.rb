325095c47c0cbd3f5a8187c58b44896e79442618
mplichta <bigcatplichta@gmail.com>
2019-12-04 13:13:30 -0700

    added collection helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,26 @@ class User < ApplicationRecord
 
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
+  # collection methods
+  # user can have toys, animals, and wishes through a keeper or a donor role
+  # keepers can also be donors, something like user.animals is ambiguous
+  # use keeper_animals and donor_animals (same as calling user.keeper.animals or user.donor.animals)
+  def donor_animals
+    if self.donor?
+      self.donor.animals
+    end
+  end
+
+  def keeper_animals
+    if self.keeper?
+      self.keeper.animals
+    end
+  end
+
+
   # role check methods
   # only needed so checking associated models return false value instead of nil
+  # still probably not needed but whatever
   def donor?
     self.donor ? true : false
   end
