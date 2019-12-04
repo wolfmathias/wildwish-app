ff40c381984bf7bbc3a446f2cdc9c5573340b96e
mplichta <bigcatplichta@gmail.com>
2019-12-04 10:52:58 -0700

    edited routes for animals and wishes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   
   # resources :donations
-  resources :wishes
+  resources :wishes, except: [:new, :show]
   # resources :toys
   resources :keepers
   # resources :donors
-  resources :animals
+  resources :animals, except: [:show]
+  
+  # nested resources for new animal wish
+  resources :animals, only: [:show] do
+    resources :wishes, only: [:new, :show]
+  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
