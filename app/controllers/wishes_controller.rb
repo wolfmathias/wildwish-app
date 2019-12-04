b1a3beebf62a26c8164b25e31d30c3cf0d0b90da
mplichta <bigcatplichta@gmail.com>
2019-12-04 10:51:39 -0700

    edited methods

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,12 +1,11 @@
 class WishesController < ApplicationController
 
     # display form for creating new wish, set animal for collection
-    def new
-
+    # route needs to be nested within animals resource
+    def new 
+        # set animal 
+        @animal = Animal.find_by(id: params[:animal_id])
     end
 
-    # to create from user filled form
-    def create
-
-    end
+    
 end
