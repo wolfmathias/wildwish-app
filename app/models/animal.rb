ac6b2dedc4f7ed8f68905284a276c71d7fc849c9
mplichta <bigcatplichta@gmail.com>
2019-12-05 12:09:31 -0700

    changed association to allow animal deletion

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,7 @@
 class Animal < ApplicationRecord
     # associations
     belongs_to :keeper
-    has_many :wishes
+    has_many :wishes, dependent: :delete_all # this needs to change so wishes other than 'new' status are archived
     has_many :toys, through: :wishes
     has_many :donations, through: :wishes
     has_many :donors, through: :donations
