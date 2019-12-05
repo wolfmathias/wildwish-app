1c2cccc1772188ed775199be36f93330d3dd5baf
mplichta <bigcatplichta@gmail.com>
2019-12-05 12:08:24 -0700

    update redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
     
     def show
-
+        if current_user.keeper?
+            redirect_to keeper_path(current_user)
+        end
     end
 
     def home
         # binding.pry
-        if user_signed_in?
-            redirect_to user_path(current_user)
-        else
+        # if user_signed_in?
+        #     redirect_to user_path(current_user)
+        # else
             render '/landing'
-        end
+        # end
     end
 
 end
