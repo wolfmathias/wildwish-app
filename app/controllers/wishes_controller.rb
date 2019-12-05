f2b6622a8a3b45f4ed3676a654f5d33b0be8cb5d
mplichta <bigcatplichta@gmail.com>
2019-12-05 12:07:48 -0700

    added authorizations and strong params

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -2,12 +2,20 @@ class WishesController < ApplicationController
 
     # display form for creating new wish, set animal for collection
     # route needs to be nested within animals resource
+    before_action :authenticate_user!
+    load_and_authorize_resource
+
+    def index
+
+    end
+
     def new 
         # set animal 
         @animal = Animal.find_by(id: params[:animal_id])
     end
 
     def edit
+
     end
 
     def show
@@ -15,7 +23,13 @@ class WishesController < ApplicationController
     end
 
     def index
-        #admin only action
+        
+    end
+
+    private
+
+    def wish_params
+        params.require(:wish).permit.(:image_urls) # actual attribute will be different once image uploading functionality is built
     end
 
 end
