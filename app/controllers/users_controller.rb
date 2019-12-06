2618022cbecb970d66c067319e17805ac27a977e
mplichta <bigcatplichta@gmail.com>
2019-12-06 16:54:42 -0700

    updated authenticate action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:home]
     
     def show
         if current_user.keeper?
