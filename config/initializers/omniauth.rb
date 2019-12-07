4eb0e7ef5c60efde75678ca859eae0736c528dd5
mplichta <bigcatplichta@gmail.com>
2019-12-06 19:54:02 -0700

    commented out method

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-end
+
+# double check what it's doing
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {:provider_ignores_state => true}
+# end
 
 # OmniAuth::Strategies::GoogleOauth2 (alternative provider name recc'd by some sources)
