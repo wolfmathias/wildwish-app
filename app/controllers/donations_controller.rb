38b103ba2e20146083a27b32665db22404aeb26d
mplichta <bigcatplichta@gmail.com>
2019-12-07 22:52:34 -0700

    added/updated views

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,17 +1,21 @@
 class DonationsController < ApplicationController
-    
+    load_and_authorize_resource only: [:index, :show]
+
+    def show
+    end
+
+    def index
+    end
+   
     def new
         # use cancan to set?
         if user_signed_in? 
             donor = current_user.set_donor
-            # @donor.save
-            
         else
             donor = Donor.new
         end
-        # donor.save
         @donation = donor.donations.build(wish_id: params[:wish_id])
-        
+
     end
 
     def create
@@ -21,7 +25,7 @@ class DonationsController < ApplicationController
         @donation = Donation.new(donation_params)
         if @donation.save
             if user_signed_in?
-                redirect_to user_path(current_user) 
+                redirect_to donations_path 
             else
                 redirect_to root_path
             end
