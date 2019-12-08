aa1937e0bd8b4910484648bc6dbe4b4e9b534e02
mplichta <bigcatplichta@gmail.com>
2019-12-08 12:53:42 -0700

    added user reader method

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,14 +1,22 @@
 class Donor < ApplicationRecord
     # associations
-    # belongs_to taken out to allow standalone creation
-    # will still have 'user_id' column
-    # belongs_to :user
     has_many :donations
     has_many :wishes, through: :donations
     has_many :toys, through: :wishes
     has_many :animals, through: :wishes
     
+    # belongs_to taken out to allow standalone creation
+    # will still have 'user_id' column
+    # use 'user' method to call on user
+
     #accepts_nested_attributes_for :donations
 
+    ## scopes
+    # scope for donors that have accounts
+    # scope :has_account, -> { where(:user_id.exists?) 
+
+    def user
+        User.find_by(id: self.user_id)
+    end
     
 end
