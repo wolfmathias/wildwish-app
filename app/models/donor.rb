49adfecf6222e535c672c0fc4757a66750f914e7
mplichta <bigcatplichta@gmail.com>
2019-12-07 21:42:51 -0700

    fixed associations

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,10 +1,14 @@
 class Donor < ApplicationRecord
     # associations
-    belongs_to :user
+    # belongs_to taken out to allow standalone creation
+    # will still have 'user_id' column
+    # belongs_to :user
     has_many :donations
     has_many :wishes, through: :donations
     has_many :toys, through: :wishes
     has_many :animals, through: :wishes
     
     #accepts_nested_attributes_for :donations
+
+    
 end
