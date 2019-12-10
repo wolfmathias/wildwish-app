8c58084471a361bcd39e85a311d801785d75383d
mplichta <bigcatplichta@gmail.com>
2019-12-09 19:43:56 -0700

    commented out addresses until model is built

--- app/models/keeper.rb
+++ app/models/keeper.rb
@@ -1,11 +1,11 @@
 class Keeper < ApplicationRecord
     # associations
     belongs_to :user
-    has_many :addresses
+    # has_many :addresses
     has_many :animals
     has_many :wishes, through: :animals
     has_many :toys, through: :wishes
     accepts_nested_attributes_for :animals
-    accepts_nested_attributes_for :addresses
+    # accepts_nested_attributes_for :addresses
 
 end
