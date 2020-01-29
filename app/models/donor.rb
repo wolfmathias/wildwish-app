836ac3db280fc9652a06cc3e13e6faa9bb4bded4
bigcatplichta <bigcatplichta@gmail.com>
2020-01-29 12:58:54 -0700

    edited donor model

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,22 +1,7 @@
 class Donor < ApplicationRecord
-    # associations
     has_many :donations
     has_many :wishes, through: :donations
     has_many :toys, through: :wishes
     has_many :animals, through: :wishes
-    
-    # belongs_to taken out to allow standalone creation
-    # will still have 'user_id' column
-    # use 'user' method to call on user
-
-    #accepts_nested_attributes_for :donations
-
-    ## scopes
-    # scope for donors that have accounts
-    # scope :has_account, -> { where(:user_id.exists?) 
-
-    def user
-        User.find_by(id: self.user_id)
-    end
-    
+    belongs_to :user, optional: true
 end
