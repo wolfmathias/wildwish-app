630d79ca2b369ef8a6b74cbec70079390e4dae1a
bigcatplichta <bigcatplichta@gmail.com>
2020-01-30 10:56:51 -0700

    updated address association

--- app/models/keeper.rb
+++ app/models/keeper.rb
@@ -2,11 +2,11 @@ class Keeper < ApplicationRecord
     # associations
     belongs_to :user, optional: true
     belongs_to :zoo, optional: true
-    # has_many :addresses
+    has_many :addresses
     has_many :animals
     has_many :wishes, through: :animals
     has_many :toys, through: :wishes
     accepts_nested_attributes_for :animals
-    # accepts_nested_attributes_for :addresses
+    accepts_nested_attributes_for :addresses
 
 end
