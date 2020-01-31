87b1ff5b14f3baa7993d77193b31b8c81ba856f6
bigcatplichta <bigcatplichta@gmail.com>
2020-01-30 21:31:27 -0700

    added column for location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_160458) do
+ActiveRecord::Schema.define(version: 2020_01_31_041027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_01_30_160458) do
     t.bigint "keeper_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["keeper_id"], name: "index_animals_on_keeper_id"
   end
 
