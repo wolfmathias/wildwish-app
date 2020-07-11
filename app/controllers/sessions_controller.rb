a82095b31ee600366fa5192d17c54761f581c472
bigcatplichta <bigcatplichta@gmail.com>
2020-07-10 20:11:52 -0600

    add method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < Devise::SessionsController
+    
+    def get_token
+        cookies[:csrf_token] form_authenticity_token
+    end
 
     def create
         puts 'Hello world'
